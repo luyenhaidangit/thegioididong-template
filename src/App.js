730d340cfaf5ceb13d0e9c95de3613e16e118c26
luyenhaidangit <106638147+luyenhaidangit@users.noreply.github.com>
2023-02-04 14:41:28 +0700

    Create base project

--- src/App.js
+++ src/App.js
@@ -1,57 +1,27 @@
+// Library
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+// Client
+import Layout from './components/Layout/Client/Layout';
+import Home from './pages/Home/Home';
+import Product from './pages/Product/Product';
+
+// Style
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path='product' element={<Product />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
