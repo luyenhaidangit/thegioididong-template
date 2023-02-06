2ccd1a8cfb0ee62801c60ce72e8443f000af206b
luyenhaidangit <106638147+luyenhaidangit@users.noreply.github.com>
2023-02-06 20:26:10 +0700

    Design base template home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,7 +2,26 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <body>
+            <div className='big-banner'>big banner</div>
+            <div className='main-content container'>
+                <div className='big-banner'>big banner</div>
+                <div className='big-campaign'>big-campaign</div>
+                <div className='option-promo'>option-promo</div>
+                <div className='flashsale'>flashsale</div>
+                <div className='hotdeal'>hotdeal</div>
+                <div className='promo-another'>promo-another</div>
+                <div className='shopping-trends'>shopping-trends</div>
+                <div className='category-feature'>category-feature</div>
+                <div className='service-conversation'>service-conversation</div>
+                <div className='category-feature'>category-feature</div>
+                <div className='daily-suggestion'>daily-suggest</div>
+                <div className='trademark '>trademark </div>
+                <div className='technology-box'>technology-box</div>
+                <div className='newchain'>newchain</div>
+                <div className='promo-bhx'>promo-bhx</div>
+            </div>
+        </body>
     )
 }
 
