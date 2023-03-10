29f0281ef7359b55ca5412fd0960dac3cb37fc01
luyenhaidangit <106638147+luyenhaidangit@users.noreply.github.com>
2023-03-10 15:12:58 +0700

    Fix bug change data slide header top api

--- src/components/Layouts/Client/HeaderTopBar.js
+++ src/components/Layouts/Client/HeaderTopBar.js
@@ -13,7 +13,7 @@ import "../../../assets/Styles/Client/Layouts/HeaderTopBar.css"
 import { GrFormNext, GrFormPrevious } from "react-icons/gr"
 
 // Api
-import { GetSlideHeaderTopClient } from '../../../apis/Client/slideApi';
+import HeaderTopBarApi from '../../../data/HeaderTopBar';
 import { baseManageURL } from '../../../helpers/Apis/baseURL';
 
 const HeaderTopBar = () => {
@@ -31,8 +31,8 @@ const HeaderTopBar = () => {
 
     // Func
     const fetchSlidesHeaderTopBar = async () => {
-        let res = await GetSlideHeaderTopClient();
-        setSlideData(res.items[0].slideItems ?? []);
+        let res = await HeaderTopBarApi;
+        setSlideData(res.items ?? []);
     }
 
     const changeBackGroundColorHeaderTopBar = (swiper) => {
@@ -42,40 +42,51 @@ const HeaderTopBar = () => {
     console.log(slideData)
 
     return (
-        <div className="header-top-bar" style={backgroundColorHeaderTopBar ? { backgroundColor: backgroundColorHeaderTopBar } : {}}>
-            <div className='container'>
-                <Swiper
-                    modules={[Navigation]}
-                    spaceBetween={50}
-                    slidesPerView={1}
-                    loop={true}
-                    speed={0}
-                    navigation={{
-                        prevEl: navigationPrevRef.current,
-                        nextEl: navigationNextRef.current,
-                    }}
-                    onBeforeInit={(swiper) => {
-                        swiper.params.navigation.prevEl = navigationPrevRef.current;
-                        swiper.params.navigation.nextEl = navigationNextRef.current;
-                    }}
-                    onSlideChange={(swiper) => changeBackGroundColorHeaderTopBar(swiper)}
-                >
-                    {
-                        slideData && slideData.length > 0 &&
-                        slideData.map((item, index) => {
-                            return (
-                                <SwiperSlide className='header-top-bar__item' key={`slide-header-top-bar-${index}`}>
-                                    <img className='header-top-bar__item-image' src={baseManageURL + item.image} alt={""} />
-                                </SwiperSlide>
-                            )
-                        })
-                    }
+        <>
+            {
+                slideData && slideData.length > 0 &&
+                slideData.map((slideDataItem, index) => {
+                    return (
+                        <div key={`header-top-bar-${index}`} className="header-top-bar" style={backgroundColorHeaderTopBar ? { backgroundColor: backgroundColorHeaderTopBar } : {}}>
+                            <div className='container'>
+                                <Swiper
+                                    modules={[Navigation]}
+                                    spaceBetween={50}
+                                    slidesPerView={1}
+                                    loop={true}
+                                    speed={0}
+                                    navigation={{
+                                        prevEl: navigationPrevRef.current,
+                                        nextEl: navigationNextRef.current,
+                                    }}
+                                    onBeforeInit={(swiper) => {
+                                        swiper.params.navigation.prevEl = navigationPrevRef.current;
+                                        swiper.params.navigation.nextEl = navigationNextRef.current;
+                                    }}
+                                    onSlideChange={(swiper) => changeBackGroundColorHeaderTopBar(swiper)}
+                                >
+                                    {
+                                        slideDataItem.slideItems && slideDataItem.slideItems.length > 0 &&
+                                        slideDataItem.slideItems.map((item, index) => {
+                                            return (
+                                                <SwiperSlide className='header-top-bar__item' key={`slide-header-top-bar-${index}`}>
+                                                    <img className='header-top-bar__item-image' src={item.image} alt={""} />
+                                                </SwiperSlide>
+                                            )
+                                        })
+                                    }
 
-                    <div ref={navigationPrevRef} className="header-top-bar__swiper-button"><GrFormPrevious className='header-top-bar__swiper-icon swiper-button-prev' /></div>
-                    <div ref={navigationNextRef} className="header-top-bar__swiper-button"><GrFormNext className='header-top-bar__swiper-icon swiper-button-next' /></div>
-                </Swiper>
-            </div>
-        </div>
+                                    <div ref={navigationPrevRef} className="header-top-bar__swiper-button"><GrFormPrevious className='header-top-bar__swiper-icon swiper-button-prev' /></div>
+                                    <div ref={navigationNextRef} className="header-top-bar__swiper-button"><GrFormNext className='header-top-bar__swiper-icon swiper-button-next' /></div>
+                                </Swiper>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+        </>
+
+
     )
 }
 
--- src/data/HeaderTopBar.js
+++ src/data/HeaderTopBar.js
@@ -1,24 +1,38 @@
-const HeaderTopBar_01 = require("../assets/Images/HeaderTopBar/DH-chung-1200-44-1200x44.png")
-const HeaderTopBar_02 = require("../assets/Images/HeaderTopBar/Khai-xuan-1200-44-1200x44.png")
-const HeaderTopBar_03 = require("../assets/Images/HeaderTopBar/thantaidd-1200-44-1200x44.png")
+const HeaderTopBar_01 = require("../assets/Images/Slide/header-top-slide-1.png")
+const HeaderTopBar_02 = require("../assets/Images/Slide/header-top-slide-2.png")
+const HeaderTopBar_03 = require("../assets/Images/Slide/header-top-slide-3.png")
 
 
-const HeaderTopBar = [
-    {
-        id: 1,
-        image: HeaderTopBar_01,
-        backgroundColor: "rgb(255, 198, 2)",
-    },
-    {
-        id: 2,
-        image: HeaderTopBar_02,
-        backgroundColor: "rgb(231, 32, 44)",
-    },
-    {
-        id: 2,
-        image: HeaderTopBar_03,
-        backgroundColor: "rgb(231, 32, 44)",
-    }
-]
+const HeaderTopBar = {
+    "items": [
+        {
+            "id": "11",
+            "name": "Header Top Bar Slide",
+            "page": "client",
+            "position": "header_top",
+            "slideItems": [
+                {
+                    "title": null,
+                    "image": HeaderTopBar_01,
+                    "url": null
+                },
+                {
+                    "title": null,
+                    "image": HeaderTopBar_02,
+                    "url": null
+                },
+                {
+                    "title": null,
+                    "image": HeaderTopBar_03,
+                    "url": null
+                }
+            ]
+        }
+    ],
+    "pageIndex": 0,
+    "pageSize": 0,
+    "totalRecords": 1,
+    "totalPages": 1
+}
 
 export default HeaderTopBar;
