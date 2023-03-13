d77c5e5400bb189c48d2146145b3b32c4f4b46c8
luyenhaidangit <106638147+luyenhaidangit@users.noreply.github.com>
2023-03-13 19:56:55 +0700

    Create component TopBanner page ProductCategory and reponsive

--- src/components/Pages/ProductCategory/TopBanner.js
+++ src/components/Pages/ProductCategory/TopBanner.js
@@ -2,9 +2,10 @@
 import React, { useRef } from 'react'
 
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation } from 'swiper';
+import { Navigation, Pagination } from 'swiper';
 import 'swiper/css';
 import "swiper/css/navigation";
+import "swiper/css/pagination"
 
 import { GrFormNext, GrFormPrevious } from "react-icons/gr"
 
@@ -13,20 +14,22 @@ import "../../../assets/Styles/Components/ProductCategory/TopBanner.css"
 
 const TopBanner = (props) => {
     const { slide } = props;
+    const { bannerFirst } = props;
+    const { bannerSecond } = props;
     const navigationPrevRef = useRef(null);
     const navigationNextRef = useRef(null);
 
-    console.log(slide);
+    console.log(bannerFirst);
 
     return (
         <div className='top-banner mt-3'>
-            <div className='container d-flex gap-3'>
+            <div className='container d-flex align-items-center gap-3'>
                 <div className='left-top-banner position-relative'>
                     {
                         slide &&
                         <>
                             <Swiper
-                                modules={[Navigation]}
+                                modules={[Navigation, Pagination]}
                                 spaceBetween={12}
                                 slidesPerView={1}
                                 loop={true}
@@ -37,29 +40,22 @@ const TopBanner = (props) => {
                                     nextEl: ".banner-top__button-product-next-1",
                                 }}
                                 pagination={{ clickable: true }}
-                            // onBeforeInit={(swiper) => {
-                            //     swiper.params.navigation.prevEl = navigationPrevRef.current;
-                            //     swiper.params.navigation.nextEl = navigationNextRef.current;
-                            // }}
-                            // breakpoints={{
-                            //     1200: {
-                            //         slidesPerView: 5,
-                            //     },
-                            //     // // when window width is >= 1024px
-                            //     992: {
-                            //         slidesPerView: 4,
-                            //     },
-                            //     768: {
-                            //         slidesPerView: 3,
-                            //     },
-                            // }}
+                                // onBeforeInit={(swiper) => {
+                                //     swiper.params.navigation.prevEl = navigationPrevRef.current;
+                                //     swiper.params.navigation.nextEl = navigationNextRef.current;
+                                // }}
+                                breakpoints={{
+                                    768: {
+
+                                    },
+                                }}
                             >
                                 {
                                     slide?.slideItems && slide?.slideItems?.length > 0 &&
                                     slide?.slideItems.map((item, index) => {
                                         return (
                                             <SwiperSlide key={index}>
-                                                <img className='w-100' src={item?.image} alt='slide' />
+                                                <img className='top-slide__item' src={item?.image} alt='slide' />
                                             </SwiperSlide>
                                         )
                                     })
@@ -72,8 +68,19 @@ const TopBanner = (props) => {
                     }
                 </div>
 
-                <div className='right-top-banner'>
-                    ok
+                <div className='right-top-banner d-flex w-100 flex-column gap-2'>
+                    <div className='top-banner'>
+                        {
+                            bannerFirst &&
+                            <img className='w-100' src={bannerFirst?.image} alt='slide' />
+                        }
+                    </div>
+                    <div className='top-banner'>
+                        {
+                            bannerSecond &&
+                            <img className='w-100' src={bannerSecond?.image} alt='slide' />
+                        }
+                    </div>
                 </div>
             </div>
         </div>
