a61dc7c8282fe88afccf91539c19dbc356f71012
luyenhaidangit <106638147+luyenhaidangit@users.noreply.github.com>
2023-04-03 01:59:01 +0700

    Fix warning Topbanner Product category

--- src/pages/ProductCategory.js
+++ src/pages/ProductCategory.js
@@ -24,7 +24,7 @@ const ProductCategory = () => {
     useEffect(() => {
         fetchTopBanner(id);
         fetchDataProductCategory();
-    }, []);
+    }, [id]);
 
     // Function
     const fetchDataProductCategory = async () => {
