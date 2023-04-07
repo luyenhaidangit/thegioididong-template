155c5c17118e2fb588e7f640907bdd4db9eb5e5e
luyenhaidangit <106638147+luyenhaidangit@users.noreply.github.com>
2023-04-08 02:25:35 +0700

    Return project

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import { RouterProvider } from 'react-router-dom';
-import router from './router';
-import { ContextProvider } from './contexts/ContextProvider';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
-root.render(
-    <ContextProvider>
-        <RouterProvider router={router} app={App} />
-    </ContextProvider>
-);
-// root.render(<App />);
+root.render(<App />);
